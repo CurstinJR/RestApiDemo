03b38a34951fd6b197327b495ef36208efb0b1fb
CurstinJR <cjrose13@protonmail.com>
2022-04-30 19:24:09 +0200

    Add Role Repository Tests
    Test the CRUD operations for the role repository
    	- save
    	- delete
    	- findall
    	- findbyid
    	- update
    
    Issues: #2

--- src/test/java/za/ac/cput/restapi/repository/RoleRepositoryTest.java
+++ src/test/java/za/ac/cput/restapi/repository/RoleRepositoryTest.java
@@ -1,9 +1,79 @@
 package za.ac.cput.restapi.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import za.ac.cput.restapi.entity.Role;
 
-// TODO: Write tests for role repository
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@DataJpaTest
 class RoleRepositoryTest
 {
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Autowired
+    TestEntityManager testEntityManager;
+
+    Role roleAdmin = new Role("ADMIN");
+    Role roleUser = new Role("USER");
+
+    @Test
+    public void saveRoleTest()
+    {
+        Role savedAdminRole = roleRepository.save(roleAdmin);
+        Role savedUserRole = roleRepository.save(roleUser);
+
+        Role existAdminRole = testEntityManager.find(Role.class, savedAdminRole.getId());
+        Role existUserRole = testEntityManager.find(Role.class, savedUserRole.getId());
+
+        assertThat(roleAdmin.getRoleName()).isEqualTo(existAdminRole.getRoleName());
+        assertThat(roleUser.getRoleName()).isEqualTo(existUserRole.getRoleName());
+    }
+
+    @Test
+    public void getRoleByIdTest()
+    {
+        Role existRole = roleRepository.findById(1L).orElseThrow();
+
+        assertThat(existRole.getRoleName()).isEqualTo("ADMIN");
+    }
+
+    @Test
+    public void getRolesTest()
+    {
+        List<Role> roles = roleRepository.findAll();
+
+        assertThat(roles.size()).isGreaterThan(0);
+    }
+
+    @Test
+    public void updateRoleTest()
+    {
+        Role existRole = roleRepository.findById(1L).orElseThrow();
+        String existRoleName = existRole.getRoleName();
+
+        existRole.setRoleName("OWNER");
+
+        Role updateRole = roleRepository.save(existRole);
+
+        assertThat(updateRole.getRoleName()).isNotEqualTo(existRoleName);
+    }
+
+    @Test
+    public void deleteRoleTest()
+    {
+        Role existRole = roleRepository.findById(1L).orElseThrow();
+
+        roleRepository.delete(existRole);
+
+        boolean roleNotExists = roleRepository.findById(1L).isPresent();
+
+        assertThat(roleNotExists).isFalse();
+    }
 
 }
